19d78c9c7204b5b38399566bb3988e34404a86ef
Tomas <tomas@adomavicius.com>
2018-10-06 00:13:35 +0200

    built all into a single docker image - WIP

--- src/backend/index.js
+++ src/backend/index.js
@@ -15,7 +15,9 @@ app.get('/', async (req, res) => {
     }
 
 });
-
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, '..', '../dist/index.html'));
+});
 app.listen(80, () => console.log(`Server is listening on port 80`));
 
 
