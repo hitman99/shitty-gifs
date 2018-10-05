3b9abe7fdcf87dd130979cc7fdebcc32bff3ec1a
Tomas <tomas@adomavicius.com>
2018-10-06 00:30:11 +0200

    Frontend is served properly

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,20 +1,24 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const Storage = require('./cloud-storage');
 const config = require('./config');
 const gcs = new Storage(!!process.env.LOCAL, config);
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, '..', '../dist/')));
 
 app.get('/healthz', (req, res) => {
     res.send('OK');
 });
-app.get('/', async (req, res) => {
-    let gifs = await gcs.getFiles();
-    if (!(gifs instanceof Error)) {
-        res.send(gifs.map(g => g.name));
-    } else {
-        res.send('Could not get list of files from cloud')
-    }
-
-});
+// app.get('/', async (req, res) => {
+//     let gifs = await gcs.getFiles();
+//     if (!(gifs instanceof Error)) {
+//         res.send(gifs.map(g => g.name));
+//     } else {
+//         res.send('Could not get list of files from cloud')
+//     }
+//
+// });
 app.get('*', function(req, res) {
   res.sendFile(path.resolve(__dirname, '..', '../dist/index.html'));
 });
